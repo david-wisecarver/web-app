b8138bda5d45b82546787b4b81ccfeb4a921c376
david-wisecarver <86679317+david-wisecarver@users.noreply.github.com>
2021-07-20 17:19:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -45,9 +45,8 @@ app.use(
     auth0Logout: true,
     baseURL: APP_URL,
     authorizationParams: {
-      response_type: "code id_token",
+      response_type: "code",
       audience: "https://expenses-api",
-      //scope: "openid profile email read:reports",
     },
   })
 );
@@ -76,11 +75,11 @@ app.get("/user", requiresAuth(), async (req, res) => {
 
 app.get("/expenses", requiresAuth(), async (req, res, next) => {
   try {
-    //const { token_type, access_token } = req.oidc.accessToken;
+    const { token_type, access_token } = req.oidc.accessToken;
     const expenses = await axios.get(`${API_URL}/reports`, {
-      //headers: {
-       // Authorization: `${token_type} ${access_token}`,
-     // },
+      headers: {
+        Authorization: `${token_type} ${access_token}`,
+      },
     });
     res.render("expenses", {
       user: req.oidc && req.oidc.user,
